20c6f08ef2f18873c34791328ef09ce9fc1b03cf
Nikhil <nikhil.kamineni@gmail.com>
2022-11-10 21:36:48 -0800

    Update wezterm font config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,21 @@
 local wezterm = require("wezterm")
 return {
-	font = wezterm.font("Iosevka Nerd Font Mono", { weight = "ExtraLight" }),
-	font_size = 18.0,
+	-- font = wezterm.font("Iosevka Nerd Font Mono", { weight = "Light" }),
+	-- font_size = 18.0,
+
+	-- font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Thin" }),
+	-- font_size = 16.0,
+  --
+	font = wezterm.font("VictorMono Nerd Font Mono", { weight = "Light" }),
+	font_size = 17.0,
+  font_rules = {
+    {
+      italic = true,
+      font = wezterm.font("VictorMono Nerd Font Mono", { weight = "Light", style = "Oblique" })
+    }
+  },
+
+  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
 	window_decorations = "RESIZE",
 	hide_tab_bar_if_only_one_tab = true,
 	adjust_window_size_when_changing_font_size = false,
