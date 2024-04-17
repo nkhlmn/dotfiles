f89bf9c434985865936c06771406c7cd2870b7a9
Nikhil <nikhil.kamineni@gmail.com>
2024-04-17 03:01:51 -0700

    update wezterm config

---
+++ wezterm/keys.lua
@@ -0,0 +1,127 @@
+local wezterm = require("wezterm")
+local action = wezterm.action
+return {
+  -- Open launcers
+  { key = 'l', mods = 'CTRL|SHIFT', action = wezterm.action.ShowLauncherArgs { flags = 'FUZZY|COMMANDS'} },
+  {
+    key = 'K',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.ShowLauncherArgs { flags = 'FUZZY|KEY_ASSIGNMENTS', title = 'Keys' },
+  },
+  {
+    key = 's',
+    mods = 'LEADER',
+    action = wezterm.action.ShowLauncherArgs { flags = 'FUZZY|WORKSPACES', title = 'Workspaces' },
+  },
+  {
+    key = 'T',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.ShowLauncherArgs { flags = 'FUZZY|TABS', title = 'Tabs' },
+  },
+
+  -- Create new workspace
+  {
+    key = 'n',
+    mods = 'LEADER|CTRL',
+    action = wezterm.action.PromptInputLine {
+      description = wezterm.format {
+        { Attribute = { Intensity = 'Bold' } },
+        { Foreground = { AnsiColor = 'Fuchsia' } },
+        { Text = 'Create new workspace' },
+      },
+      action = wezterm.action_callback(function(window, pane, line)
+        if line then
+          window:perform_action(
+            wezterm.action.SwitchToWorkspace { name = line },
+            pane
+          )
+        end
+      end),
+    },
+  },
+
+  -- Rename workspace
+  {
+    key = 'r',
+    mods = 'LEADER|CTRL',
+    action = wezterm.action.PromptInputLine {
+      description = wezterm.format {
+        { Attribute = { Intensity = 'Bold' } },
+        { Foreground = { AnsiColor = 'Blue' } },
+        { Text = 'Rename workspace' }
+      },
+      action = wezterm.action_callback(function(window, pane, line)
+        if line then
+          wezterm.mux.rename_workspace(
+            wezterm.mux.get_active_workspace(),
+            line
+          )
+        end
+      end),
+    },
+  },
+
+  -- Rename tab
+  {
+    key = 't',
+    mods = 'LEADER|CTRL',
+    action = wezterm.action.PromptInputLine {
+      description = wezterm.format {
+        { Attribute = { Intensity = 'Bold' } },
+        { Foreground = { AnsiColor = 'Blue' } },
+        { Text = 'Rename tab' }
+      },
+      action = wezterm.action_callback(function(window, pane, line)
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end),
+    },
+  },
+
+  -- Split pane
+  {
+    key = 'v',
+    mods = 'LEADER|CTRL',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 's',
+    mods = 'LEADER|CTRL',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+
+  -- Close pane
+  {
+    key = 'w',
+    mods = 'CMD',
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
+  {
+    key = 'w',
+    mods = 'LEADER|CTRL',
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
+
+  -- Select pane
+  {
+    key = 'h',
+    mods = 'LEADER|CTRL',
+    action = action.ActivatePaneDirection 'Left',
+  },
+  {
+    key = 'l',
+    mods = 'LEADER|CTRL',
+    action = action.ActivatePaneDirection 'Right',
+  },
+  {
+    key = 'k',
+    mods = 'LEADER|CTRL',
+    action = action.ActivatePaneDirection 'Up',
+  },
+  {
+    key = 'j',
+    mods = 'LEADER|CTRL',
+    action = action.ActivatePaneDirection 'Down',
+  },
+}
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,27 +1,25 @@
 local wezterm = require("wezterm")
+
+wezterm.on('update-status', function(window, pane)
+  window:set_left_status(wezterm.format {
+    { Attribute = { Intensity = 'Bold' } },
+    { Text = ' ' .. wezterm.mux.get_active_workspace() .. ' ' },
+  })
+end)
+
 return {
-	-- font = wezterm.font("Iosevka Nerd Font Mono", { weight = "Light" }),
-	-- font_size = 18.0,
-
-	-- font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Thin" }),
-	-- font_size = 16.0,
-  --
-	font = wezterm.font("VictorMono Nerd Font Mono", { weight = "Light" }),
-	font_size = 17.0,
-  font_rules = {
-    {
-      italic = true,
-      font = wezterm.font("VictorMono Nerd Font Mono", { weight = "Light", style = "Oblique" })
-    }
-  },
-
-  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
-	window_decorations = "RESIZE",
-	hide_tab_bar_if_only_one_tab = true,
-	adjust_window_size_when_changing_font_size = false,
-	audible_bell = "Disabled",
-	color_scheme = "kanagawa",
-	-- color_scheme = "gruvbox_material_dark_hard",
-	force_reverse_video_cursor = true,
-	color_schemes = require("color_schemes"),
+  font = wezterm.font("Iosevka"),
+  -- font = wezterm.font("Iosevka", { weight = "Light" }),
+  font_size = 18.0,
+  command_palette_font_size = 16.0,
+  command_palette_bg_color = "Black",
+  command_palette_fg_color = "#dcd7ba",
+  window_decorations = "RESIZE",
+  use_fancy_tab_bar = false,
+  adjust_window_size_when_changing_font_size = false,
+  audible_bell = "Disabled",
+  force_reverse_video_cursor = true,
+  color_scheme = "Kanagawa (Gogh)",
+  leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 },
+  keys = require("keys"),
 }
