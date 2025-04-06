bb365282f93b2c30d9f3f0e2d3ce6bf034f63ace
Nikhil <nikhil.kamineni@gmail.com>
2025-04-06 11:49:04 -0700

    nvim: remove unneeded nvim-treesitter dependencies

--- nvim/lua/config/plugins/treesitter.lua
+++ nvim/lua/config/plugins/treesitter.lua
@@ -1,10 +1,6 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  dependencies = {
-    { 'nvim-treesitter/nvim-treesitter-context' },
-    { 'nvim-treesitter/nvim-treesitter-refactor' },
-  },
   main = 'nvim-treesitter.configs',
   opts = {
     highlight = { enable = true },
