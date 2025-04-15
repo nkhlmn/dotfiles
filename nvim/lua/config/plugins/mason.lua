760780f20abe486e74698fd1d634fdeaa37566b0
Nikhil <nikhil.kamineni@gmail.com>
2025-04-14 22:33:33 -0700

    nvim(mason): add handling for legacy lspconfig's

--- nvim/lua/config/plugins/mason.lua
+++ nvim/lua/config/plugins/mason.lua
@@ -5,9 +5,15 @@ return {
     'williamboman/mason-lspconfig.nvim',
     dependencies = { 'neovim/nvim-lspconfig' },
     config = function()
-      vim.lsp.enable(
-        require('mason-lspconfig').get_installed_servers()
-      )
+      local installed_servers = require('mason-lspconfig').get_installed_servers()
+      for _, server in ipairs(installed_servers) do
+        local config = require('lspconfig.configs.' .. server)
+        if vim.lsp.config[server] == nil and config ~= nil then
+          vim.lsp.config(server, config.default_config)
+        end
+      end
+
+      vim.lsp.enable(installed_servers)
     end
   },
 }
