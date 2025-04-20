7fb2b049e12f5c099d771fc0cdc340616bb32686
Nikhil <nikhil.kamineni@gmail.com>
2025-04-20 11:08:34 -0700

    nvim(mason): simplify config

--- nvim/lua/config/plugins/mason.lua
+++ nvim/lua/config/plugins/mason.lua
@@ -7,11 +7,7 @@ return {
     config = function()
       local installed_servers = require('mason-lspconfig').get_installed_servers()
       for _, server in ipairs(installed_servers) do
-        if vim.lsp.config[server] ~= nil then
-          vim.lsp.enable(server)
-        else
-          require('lspconfig')[server].setup({})
-        end
+        vim.lsp.enable(server)
       end
     end
   },
