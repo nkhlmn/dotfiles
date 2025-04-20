13c0d66e84dc134ad261cbe9c1cc464f6a144384
Nikhil <nikhil.kamineni@gmail.com>
2025-04-20 11:07:46 -0700

    nvim(blink): simplify config

--- nvim/lua/config/plugins/blink.lua
+++ nvim/lua/config/plugins/blink.lua
@@ -9,17 +9,7 @@ return {
     completion = {
       documentation = {
         auto_show = true,
-        auto_show_delay_ms = 500,
       },
-      list = {
-        selection = {
-          preselect = false,
-          auto_insert = true,
-        },
-      },
-    },
-    appearance = {
-      use_nvim_cmp_as_default = true,
     },
     sources = {
       default = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev' },
@@ -35,10 +25,4 @@ return {
   },
 
   opts_extend = { 'sources.default' },
-
-  -- config = function()
-  --   vim.lsp.config('*', {
-  --     capabilities = require('blink.cmp').get_lsp_capabilities(),
-  --   })
-  -- end
 }
