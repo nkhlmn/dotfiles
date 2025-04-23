8eb54dc44de85df164a555dcbc7ca57a44061f35
Nikhil <nikhil.kamineni@gmail.com>
2025-04-22 22:54:40 -0700

    nvim: update default colorscheme

--- nvim/lua/config/colorscheme.lua
+++ nvim/lua/config/colorscheme.lua
@@ -5,7 +5,7 @@ local g = vim.g
 o.termguicolors = true
 o.background = 'dark'
 
-local colorscheme = g.local_colorscheme or 'kanagawa'
+local colorscheme = g.local_colorscheme or 'kanagawa-dragon'
 
 -- Set the colorscheme
 vim.cmd.colorscheme(colorscheme)
