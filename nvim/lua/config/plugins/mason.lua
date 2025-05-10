480272a70bc7b70d1e9dfb615b08fcf7e79a46c1
Nikhil <nikhil.kamineni@gmail.com>
2025-05-09 19:32:39 -0700

    nvim(fix): support mason 2.0

--- nvim/lua/config/plugins/mason.lua
+++ nvim/lua/config/plugins/mason.lua
@@ -1,14 +1,9 @@
 return {
-  { 'williamboman/mason.nvim', build = ':MasonUpdate', config = true },
+  { 'mason-org/mason.nvim', opts = {} },
 
   {
-    'williamboman/mason-lspconfig.nvim',
+    'mason-org/mason-lspconfig.nvim',
     dependencies = { 'neovim/nvim-lspconfig' },
-    config = function()
-      local installed_servers = require('mason-lspconfig').get_installed_servers()
-      for _, server in ipairs(installed_servers) do
-        vim.lsp.enable(server)
-      end
-    end
+    opts = {}
   },
 }
