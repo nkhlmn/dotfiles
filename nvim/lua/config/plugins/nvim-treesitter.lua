1f0124796616fb0c7525c407a4eaf92eb1a8befa
Nikhil <nikhil.kamineni@gmail.com>
2025-05-12 21:19:52 -0700

    nvim(treesitter): add refactor/context modules

--- nvim/lua/config/plugins/nvim-treesitter.lua
+++ nvim/lua/config/plugins/nvim-treesitter.lua
@@ -2,7 +2,15 @@ return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
   main = 'nvim-treesitter.configs',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter-refactor',
+    'nvim-treesitter/nvim-treesitter-context',
+  },
   opts = {
     highlight = { enable = true },
+    context = { enable = true },
+    refactor = {
+      highlight_definitions = { enable = true, clear_on_cursor_move = false },
+    },
   },
 }
