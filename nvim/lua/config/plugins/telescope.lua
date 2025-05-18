750ea4edbc7bd730ed76e7ecc5c5d74f48034566
Nikhil <nikhil.kamineni@gmail.com>
2025-05-17 20:14:40 -0700

    nvim(telescope): use nightly

--- nvim/lua/config/plugins/telescope.lua
+++ nvim/lua/config/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
   'nvim-telescope/telescope.nvim',
-  branch = '0.1.x',
+  -- branch = '0.1.x',
   dependencies = {
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     { 'nvim-lua/plenary.nvim' }
