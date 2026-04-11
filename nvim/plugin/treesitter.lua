f27f8b8a7fed9aa77dffbee892847842b0311f45
nkhl <nkhl@nbk-mba-2026.local>
2026-04-11 15:45:52 -0700

    nvim: fix treesiter enabling

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -4,3 +4,18 @@ vim.pack.add({
     build = ':TSUpdate',
   }
 })
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = require('nvim-treesitter').get_installed(),
+  callback = function()
+    -- -- folding
+    vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    vim.wo[0][0].foldmethod = 'expr'
+
+    -- -- indenting
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+
+    -- highlighting
+    vim.treesitter.start()
+  end,
+})
